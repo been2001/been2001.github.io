ccfeb30232f99abc7d412389cf807dbf78d72a56
been2001 <been2001@yahoo.com>
2016-12-12 17:28:10 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+var main = function() { 
+    $('.icon-menu').click(function()
+    $('.menu').animate({ left: '0px'.
+    }, 200);
+    
+    $('body').animate({
+        left:'285px'
+        
+    },   200);
+};
+    
+};
+    $('.icon-close').clck()
+    $('.click').animate({left:'-285px'.
+    }, 0);
+        
+    
+    
+
+$(document).ready(main);
